2426e34126dfad206c5b6d22b986d4aaf38851c9
Miguel <migueljaimes1405@gmail.com>
2020-01-14 15:26:49 +0100

    Agregando Modulos II
    Registro y herraminetas

---
+++ SweetHealth/Utils/Literals.swift
@@ -0,0 +1,13 @@
+//
+//  Literals.swift
+//  SweetHealth
+//
+//  Created by Miguel Jaimes on 14/01/2020.
+//  Copyright © 2020 Miguel Jaimes. All rights reserved.
+//
+
+import Foundation
+
+open class Literals{
+    
+}
