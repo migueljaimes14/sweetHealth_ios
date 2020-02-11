179fe5c54c68b3f4868648751c70473f37fe7edb
Miguel <migueljaimes1405@gmail.com>
2020-02-11 19:38:04 +0100

    Home

---
+++ SweetHealth/Home/Controller/HomeTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  HomeTableViewController.swift
+//  SweetHealth
+//
+//  Created by Miguel Jaimes on 11/02/2020.
+//  Copyright © 2020 Miguel Jaimes. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+       
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      
+        return 0
+    }
+   
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "HomeTableViewCell"
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? HomeTableViewCell else {
+            fatalError("La celda no es una instancia de HomeCell")
+        }
+        return cell
+    }
+    
+    
+
+}
